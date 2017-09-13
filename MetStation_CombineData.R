e17cdebe1e4d567f07eef3a172b46ad5e997341c
Drew Duckett <duckettdj@g.cofc.edu>
2017-09-13 09:42:00 -0500

    Added code to compile data from multiple dates
    Code will add most recent data to growing compilation CSV file

--- MetStation_CombineData.R
+++ MetStation_CombineData.R
@@ -1,5 +1,5 @@
 
-
+#Combine data for one date
 setwd("~/Forest_Ecology/Met_Station_Data/6.23.17") #Set working directory to data file location
 A1 <- read.csv(dir(pattern = "^Rollinson_A1"), skip = 1, na.strings=c("-888.9")) #read in data file, change error terms to NA
 B5 <- read.csv(dir(pattern = "^Rollinson_B5"), skip = 1, na.strings=c("-888.9")) #read in data file, change error terms to NA
@@ -22,3 +22,28 @@ colnames(all_plots) <- c("Plot", "Date_Time", "Soil_Temp", "Soil_Moisture", "PAR
 csv_name <- sprintf("Met_Stations_%s%s", obs_date, ".csv") #Create filename
 
 write.csv(all_plots, file = csv_name) #Write CSV to current directory
+
+
+
+#Add to prexisting compilation CSV
+setwd("~/Forest_Ecology/Met_Station_Data/6.23.17") #Set working directory to most recent combination CSV location
+add_plot <- read.csv(dir(pattern = "^Met_Stations")) #read in most recent combination CSV data file
+
+setwd("..") #set working directory to parent folder where compilation CSV is located
+
+csvs <- file.info(list.files(pattern = "*.csv")) #get info for CSV files in directory
+csvs <- csvs[with(csvs, order(mtime)),] #sort info by date file was last modified
+csvs_last <- rownames(csvs)[length(rownames(csvs))] #get name of most recent file
+
+comp_csv <- read.csv(csvs_last) #read in latest compilation data file
+
+new_comp <- rbind.data.frame(comp_csv, add_plot) # Combine the two CSVs
+
+comp_date <- Sys.Date() #Get today's date
+comp_name <- sprintf("MetStation_2017_EW_%s%s", comp_date, ".csv") #create file name
+
+write.csv(new_comp, file = comp_name) #write compilation csv
+
+
+
+
