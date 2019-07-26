6af2520d086f914870b92ddd9e9217123800f426
Bethany Zumwalde <bzumwalde@mortonarb.org>
2019-07-26 10:22:05 -0500

    Updating QAQC script
    Ran the current short script and cleaned up a little bit.

--- Scripts/Met_QAQC.R
+++ Scripts/Met_QAQC.R
@@ -1,14 +1,17 @@
 #QAQC
 
+#Loading in libraries
 library(ggplot2)
 library(plotly)
 
+#Setting working directory
+setwd("C:/Users/BZumwalde/Desktop/ForestEcology_R/Met_Stations/Data_raw_inputs")
 
-setwd("C:/Users/BZumwalde/Desktop/ForestEcology_R/Met_Stations/Data_raw_inputs/Single_Plots")
-
-met.all <- read.csv("Met_Stations_Single_Plots.csv")
+#Reading in compiled met data created from script MetData_ConsolidatingRawData.R 
+met.all <- read.csv("Met_Stations_Compiled_2019-07-16.csv")
 met.all <- met.all[,2:ncol(met.all)]
 
+#Getting rid of erroneous values and setting to NA
 met.all[!is.na(met.all$Soil_Temp) & (met.all$Soil_Temp< -888 | met.all$Soil_Temp>999), "Soil_Temp"] <- NA
 met.all[!is.na(met.all$Soil_Moisture) & (met.all$Soil_Moisture< -0.5| met.all$Soil_Moisture>1), "Soil_Moisture"] <- NA
 met.all[!is.na(met.all$PAR) & (met.all$PAR< -888 | met.all$PAR>999), "PAR"] <- NA
@@ -27,10 +30,9 @@ names(met.stack) <- c("values", "var")
 met.stack[,c("PlotName", "Date_Time")] <- met.all[,c("PlotName", "Date_Time")]
 summary(met.stack)
 
+#Initial plot to just view the data as is
 ggplot(met.stack, aes(x = Date_Time, y = values)) +
   facet_wrap(~var, scales="free_y") +
   geom_line(aes(color=PlotName)) +
   theme_bw()
-
-
 
