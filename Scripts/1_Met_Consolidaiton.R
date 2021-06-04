97b83254ef14b00a382d3ff2e11bffd74f501e15
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-06-04 11:59:06 -0500

    Allowing Meter data to plot for dates without Hoboware data

--- Scripts/1_Met_Consolidaiton.R
+++ Scripts/1_Met_Consolidaiton.R
@@ -56,6 +56,9 @@ colnames(B127.MET) <- c("Time_MET"	, "PAR_MET", "mm Precipitation", "Lightning A
                         "% Battery Percent", "mV Battery Voltage", "kPa Reference Pressure", "°C Logger Temperature")
 
 B127.MET$File_Name <- "Meter"
+
+B127.MET <- subset(B127.MET, select = c("Time_MET",  "Air_Temp_MET", "Relative_Humidity_MET", "PAR_MET",
+                                        "Soil_Moisture_MET", "Soil_Temp_MET", "File_Name"))
                     
                     #Hoboware sensors and data columns
                     #Variable A-C are used for the same variable but different loggers
@@ -63,7 +66,7 @@ colnames(B127.HB) <- c("Row_Num", "Time5_A", "Soil_Temp_A", "Soil_Moisture_A", "
                     "Time5_B", "Soil_Temp_B", "Soil_Moisture_B","Air_Temp_B", "Relative_Humidity_B","PAR_B", "PAR_C", "Time6", 
                     "Soil_Temp_C", "Air_Temp_C")
 
-B127 <- full_join(B127.MET, B127.HB)
+B127 <- full_join(B127.HB, B127.MET)
 
 #Consolidating columns of Fahrenheit temperature and then converting it to Celcius
 #Need a solution for the grwoing number of "timestamp columns" that will increase every time I make a new pull from the stations
@@ -71,21 +74,19 @@ B127.convert <- B127 %>% mutate(Soil_Temp_X = ifelse(is.na(Soil_Temp_A), Soil_Te
                                 Air_Temp_X = ifelse(is.na(Air_Temp_A), Air_Temp_B, Air_Temp_A),
                                 PAR_B = ifelse(is.na(PAR_B), PAR_C, PAR_B),
                                 PAR = ifelse(is.na(PAR_A), PAR_B, PAR_A),
-                                Soil_Moisture_A = ifelse(is.na(Soil_Moisture_A), Soil_Moisture_B, Soil_Moisture_A),
+                                Soil_Moisture = ifelse(is.na(Soil_Moisture_A), Soil_Moisture_B, Soil_Moisture_A),
                                 Soil_Temp_Y = ifelse((Soil_Temp_X > 800 | Soil_Temp_X < -800), Soil_Temp_X, ((Soil_Temp_X-32)*(5/9))), 
                                 Air_Temp_Y = ifelse((Air_Temp_X > 800 | Soil_Temp_X < -800), Air_Temp_X, ((Air_Temp_X-32)*(5/9))))
 
 #Consolidating redundant columns:
 B127.mod <- B127.convert %>% mutate(Time5 = ifelse(is.na(Time5_A), as.character(Time5_B), as.character(Time5_A)),
                                     Relative_Humidity = ifelse(is.na(Relative_Humidity_A), Relative_Humidity_B, Relative_Humidity_A),
-                                    Soil_Temp_Z = ifelse(is.na(Soil_Temp_Y), Soil_Temp_C, Soil_Temp_Y),
-                                    Air_Temp_Z = ifelse(is.na(Air_Temp_Y), Air_Temp_C, Air_Temp_Y),
+                                    Soil_Temp = ifelse(is.na(Soil_Temp_Y), Soil_Temp_C, Soil_Temp_Y),
+                                    Air_Temp = ifelse(is.na(Air_Temp_Y), Air_Temp_C, Air_Temp_Y),
                                     
                                     #PAR = ifelse(is.na(PAR_MET), PAR_A, PAR_MET),
                                     #Combinding the converted (to celsius) hoboware with the Meter
-                                    Soil_Temp = ifelse(is.na(Soil_Temp_MET), Soil_Temp_Z, Soil_Temp_MET),
-                                    Soil_Moisture = ifelse(is.na(Soil_Moisture_MET), Soil_Moisture_A, Soil_Moisture_MET),
-                                    Air_Temp = ifelse(is.na(Air_Temp_MET), Air_Temp_Z, Air_Temp_MET))
+                                    )
 Plot.title <- "B127"
 B127.mod $ Plot_Name <- Plot.title
 #Checking columns to delete are correct for next lines
@@ -96,13 +97,20 @@ B127.mod$Time5 <- as.POSIXct(strptime(B127.mod$Time5, format="%m/%d/%y %I:%M:%S 
 B127.mod$Time_MET <- as.POSIXct(strptime(B127.mod$Time_MET, format="%m/%d/%Y %H"))
 B127.mod$Date_Check <- B127.mod$Time5
 B127.mod[is.na(B127.mod$Date_Check) & is.na(B127.mod$Time_MET) ,  "Date_Check"] <- B127.mod[is.na(B127.mod$Date_Check) & is.na(B127.mod$Time_MET), "Time6"] + 60*60
+B127.mod[is.na(B127.mod$Date_Check) ,  "Date_Check"] <- B127.mod[is.na(B127.mod$Date_Check), "Time_MET"]
 B127.mod <- transform(B127.mod, Date_Time = round.POSIXt(Date_Check, units = c("hours")))
 
-#B127.mod$Soil_Temp_MET <- NA
-#B127.mod$Soil_Moisture_MET <- NA
+#Making sure we use the new meter data when we run out of hoboware
+B127.mod$Soil_Moisture <- ifelse(is.na(B127.mod$Soil_Moisture_MET), B127.mod$Soil_Moisture, B127.mod$Soil_Moisture_MET)
+B127.mod$Soil_Temp <- ifelse(is.na(B127.mod$Soil_Temp_MET), B127.mod$Soil_Temp, B127.mod$Soil_Temp_MET)
+B127.mod$Relative_Humidity <- ifelse(is.na(B127.mod$Relative_Humidity_MET), B127.mod$Relative_Humidity, B127.mod$Relative_Humidity_MET)
+B127.mod$PAR <- ifelse(is.na(B127.mod$PAR_MET), B127.mod$PAR, B127.mod$PAR_MET)
+B127.mod$Air_Temp <- ifelse(is.na(B127.mod$Air_Temp_MET), B127.mod$Air_Temp, B127.mod$Air_Temp_MET)
 
 #For B127 the Meter soil data automatically takes over since there isn't overlap
-B127.mod <- subset(B127.mod, select = c("Date_Time", "Date_Check", "Soil_Temp", "Soil_Temp_MET", "Air_Temp", "Soil_Moisture", "Soil_Moisture_MET", "Relative_Humidity", "PAR", "Plot_Name"))
+B127.mod <- subset(B127.mod, select = c("Date_Time", "Date_Check", "Soil_Temp",  "Air_Temp", "Soil_Moisture", "Relative_Humidity", "PAR",
+                                        "Plot_Name", "Soil_Moisture_MET", "Soil_Temp_MET", "File_Name"))
+
 
 #-------------------------------------#
 #Consolidating N115 data#
@@ -186,11 +194,12 @@ N115.mod <- N115.convert %>% mutate(
 Plot.title <- "N115"                            
 N115.mod $ Plot_Name <- Plot.title
 colnames(N115.mod)
-N115.mod <- subset(N115.mod, select = c("Date_Time", "Date_Check", "Soil_Temp", "Soil_Temp_MET", "Air_Temp", "Soil_Moisture", "Soil_Moisture_MET", "Relative_Humidity", "PAR", "Plot_Name"))
-
+N115.mod <- subset(N115.mod, select = c("Date_Time", "Date_Check", "Soil_Temp",  "Air_Temp", "Soil_Moisture", "Relative_Humidity", "PAR",
+                                        "Plot_Name", "Soil_Moisture_MET", "Soil_Temp_MET", "File_Name"))
 #Having the meter soil data overwrite the hoboware if we have meter data
 N115.mod$Soil_Moisture <- ifelse(is.na(N115.mod$Soil_Moisture_MET), N115.mod$Soil_Moisture, N115.mod$Soil_Moisture_MET)
 N115.mod$Soil_Temp <- ifelse(is.na(N115.mod$Soil_Temp_MET), N115.mod$Soil_Temp, N115.mod$Soil_Temp_MET)
+N115.mod $ Plot_Name <- Plot.title
 #--------------------------------#
 
 #Consolidating HH115 data
@@ -280,12 +289,14 @@ HH115.mod <- HH115.convert %>% mutate(
 Plot.title <- "HH115"
 HH115.mod $ Plot_Name <- Plot.title
 colnames(HH115.mod)
-HH115.mod <- subset(HH115.mod, select = c("Date_Time", "Date_Check", "Soil_Temp", "Soil_Temp_MET", "Air_Temp", "Soil_Moisture", "Soil_Moisture_MET", "Relative_Humidity", "PAR", "Plot_Name"))
+HH115.mod <- subset(HH115.mod, select = c("Date_Time", "Date_Check", "Soil_Temp", "Air_Temp", "Soil_Moisture", "Relative_Humidity", "PAR", "Plot_Name"))
 
+#Extra steps to have the MET soil data replace hoboware data
+HH115.comb <- merge(HH115.mod, HH115.mid, by.x = "Date_Time", by.y = "Time_MET", all.x = T, all.y = T)
 
-HH115.mod$Soil_Moisture <- ifelse(is.na(HH115.mod$Soil_Moisture_MET), HH115.mod$Soil_Moisture, HH115.mod$Soil_Moisture_MET)
-HH115.mod$Soil_Temp <- ifelse(is.na(HH115.mod$Soil_Temp_MET), HH115.mod$Soil_Temp, HH115.mod$Soil_Temp_MET)
-
+HH115.comb$Soil_Moisture <- ifelse(is.na(HH115.comb$Soil_Moisture), HH115.comb$Soil_Moisture_MET, HH115.comb$Soil_Moisture)
+HH115.comb$Soil_Temp <- ifelse(is.na(HH115.comb$Soil_Temp), HH115.comb$Soil_Temp_MET, HH115.comb$Soil_Temp)
+HH115.comb $ Plot_Name <- Plot.title
 #-------------------------------------#
 #Consolidating U134 data
 U134.HB <-read_bulk(directory = "U134", extension = ".csv", header = TRUE, skip=1, na.strings=c("-888.9")) # Combine all data
@@ -365,16 +376,25 @@ U134.mod <- U134.convert %>% mutate(Soil_Moisture_B = ifelse(is.na(Soil_Moisture
 Plot.title <- "U134"
 U134.mod $ Plot_Name <- Plot.title
 colnames(U134.mod)
-U134.mod <- subset(U134.mod, select = c("Date_Time", "Date_Check", "Soil_Temp", "Soil_Temp_MET", "Air_Temp", "Soil_Moisture", "Soil_Moisture_MET", "Relative_Humidity", "PAR", "Plot_Name"))
+U134.mod <- subset(U134.mod, select = c("Date_Time", "Date_Check", "Soil_Temp", "Air_Temp", "Soil_Moisture", "Relative_Humidity", "PAR", "Plot_Name"))
 
-U134.mod$Soil_Moisture <- ifelse(is.na(U134.mod$Soil_Moisture_MET), U134.mod$Soil_Moisture, U134.mod$Soil_Moisture_MET)
-U134.mod$Soil_Temp <- ifelse(is.na(U134.mod$Soil_Temp_MET), U134.mod$Soil_Temp, U134.mod$Soil_Temp_MET)
+#This is removing oddly dated (Follow a different date format) values that are repeats of previous measurements
+U134.mod <- U134.mod[!is.na(U134.mod$Date_Time),]
 
+#Extra steps to have the MET soil data replace hoboware data
+#This is not done for N115 or B127 as they don't have overlap issue
+U134.comb <- merge(U134.mod, U134.mid, by.x = "Date_Time", by.y = "Time_MET", all.x = T, all.y = T)
+
+U134.comb$Soil_Moisture <- ifelse(is.na(U134.comb$Soil_Moisture), U134.comb$Soil_Moisture_MET, U134.comb$Soil_Moisture)
+U134.comb$Soil_Temp <- ifelse(is.na(U134.comb$Soil_Temp), U134.comb$Soil_Temp_MET, U134.comb$Soil_Temp)
+U134.comb$Plot_Name <- Plot.title
 #--------------------------------#
 #After running one of the above plots you run these lines.
 #I wil fix this to be a loop in some way down the line
 
-comb_plot <- rbind(B127.mod, N115.mod, HH115.mod, U134.mod)
+comb_plot <- rbind(B127.mod, N115.mod, HH115.comb, U134.comb)
+
+comb_plot <- comb_plot[!is.na(comb_plot$Plot_Name),]
 
 for(PLOT in unique(comb_plot$Plot_Name)){
   one_plot <- comb_plot[comb_plot$Plot_Name == PLOT,]
@@ -383,7 +403,7 @@ for(PLOT in unique(comb_plot$Plot_Name)){
   #Getting rid of extra time5 and time6 columns in front
 
   #Getting rid of redundant dates of data collection#
-  one_plot <- one_plot[!duplicated(one_plot[c('Date_Check')]),]
+  one_plot <- one_plot[!duplicated(one_plot[c('Date_Time')]),]
   
   #Adding in missing times so missing data can be seen
   #Defining the first and last date
@@ -408,6 +428,7 @@ for(PLOT in unique(comb_plot$Plot_Name)){
   one_plot.loop$Soil_Moisture <- as.numeric(one_plot.loop$Soil_Moisture)
   one_plot.loop$Soil_Temp <- as.numeric(one_plot.loop$Soil_Temp)
   one_plot.loop$Air_Temp <- as.numeric(one_plot.loop$Air_Temp)
+  one_plot.loop$Relative_Humidity <- as.numeric(one_plot.loop$Relative_Humidity)
   
   
   #Marking NA values as NA
