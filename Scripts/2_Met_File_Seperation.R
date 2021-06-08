66d6fb6d57e9dda62a5b78c4e074255f3cb0c5b6
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2021-06-08 11:53:42 -0500

    Updated so we only add/update the neccessary files instead of all the historic ones as well

--- Scripts/2_Met_File_Seperation.R
+++ Scripts/2_Met_File_Seperation.R
@@ -20,54 +20,99 @@ library(tidyr)
 path.personal <- "C:/Users/lfitzpatrick"
 path.data <- "/GitHub/Met_Stations/Data_raw_inputs/Single_Plots"
 path.met <- "G:/My Drive/East Woods/Rollinson_Monitoring/Data/Met Stations/Single_Plots/"
-setwd(path.met)
-path.out <- paste(path.met, "Data_clean/", Plot.title, sep="")
+path.out <- paste(path.met, "Data_clean/", sep="")
+setwd(path.out)
+
 
 #Currently using the Month_Separation script because combining the seperate files results in 147 columns
 #Will probably seperate the current month script into a QAQC script and the seperation script
 
 # Seperating by chosen year and month values
-month.check = 0
-rows=1
-for (i in rows:nrow(one_plot.loop)){
-  Date.month <- one_plot.loop[i, "Date_Time"]
-  month.extract <- month(Date.month)
-  if (!is.na(month.extract)){
-    if (month.extract != month.check){
-      if(month.extract == 1 | month.extract == 3 | month.extract == 5 | 
-         month.extract == 7 | month.extract == 8 | month.extract == 10 | month.extract == 12){
-        last.day = "31" 
-      } else if(month.extract == 4 | month.extract == 6 | month.extract == 9 | month.extract == 11){
-        last.day = "30"
-      } else if(month.extract == 2) {last.day = "28"}
-      month.file <- ifelse(nchar(month.extract) == 2, month.extract, paste("0", month.extract, sep=""))
-      Date.min <- paste(year(Date.month), "-", month.extract, "-01 00:00:00", sep="")
-      Date.max <- paste(year(Date.month), "-", month.extract, "-", last.day, " 23:59:59", sep="")
-      one_plot.month <- subset(one_plot.loop, Date_Time >= as.POSIXlt(Date.min) & Date_Time <= as.POSIXlt(Date.max))
-      filename <- paste(Plot.title,"-", year(Date.month), "-", month.file, ".csv", sep = "")
-      write.csv(one_plot.month, file.path(path.out,  file = filename), row.names = FALSE)
-      rows = rows + nrow(one_plot.month)
-      if(month.check == 12) month.check = 1 else(month.check = (month.check + 1))
+
+plot.B127 <- read.csv("B127/B127.csv")
+plot.N115 <- read.csv("N115/N115.csv")
+plot.HH115 <- read.csv("HH115/HH115.csv")
+plot.U134 <- read.csv("U134/U134.csv")
+
+
+comb <- rbind(plot.B127, plot.N115, plot.HH115, plot.U134)
+comb <- comb[!is.na(comb$Plot_Name),]
+
+for(PLOT in unique(comb$Plot_Name)){
+  month.check = 0
+  rows=1
+  one_plot.loop <- comb[comb$Plot_Name == PLOT,]
+  
+  #Making sure we only update and add the neccessary files
+  old.files <- dir(file.path(path.met,"Data_clean" ,PLOT), ".csv")
+  
+  split <- strsplit(old.files, "_")
+  
+  split <- lapply(split, function (x) x[2])
+  
+  date <- unlist(lapply(split, function (x) sub(".csv", "", x)))
+  
+  date <- as.Date(date)
+  
+  latest <- max(date, na.rm = T)
+  
+  one_plot.loop <- one_plot.loop[one_plot.loop$Date_Time >= latest, ]
+  
+  for (i in rows:nrow(one_plot.loop)){
+    Date.month <- one_plot.loop[i, "Date_Time"]
+    month.extract <- month(Date.month)
+    if (!is.na(month.extract)){
+      if (month.extract != month.check){
+        if(month.extract == 1 | month.extract == 3 | month.extract == 5 | 
+           month.extract == 7 | month.extract == 8 | month.extract == 10 | month.extract == 12){
+          last.day = "31" 
+        } else if(month.extract == 4 | month.extract == 6 | month.extract == 9 | month.extract == 11){
+          last.day = "30"
+        } else if(month.extract == 2) {last.day = "28"}
+        
+        
+        month.file <- ifelse(nchar(month.extract) == 2, month.extract, paste("0", month.extract, sep=""))
+        Date.min <- paste(year(Date.month), "-", month.extract, "-01 00:00:00", sep="")
+        Date.max <- paste(year(Date.month), "-", month.extract, "-", last.day, " 23:59:59", sep="")
+        
+        
+        one_plot.month <- subset(one_plot.loop, Date_Time >= as.POSIXlt(Date.min) & Date_Time <= as.POSIXlt(Date.max))
+        filename <- paste(PLOT,"_", year(Date.month), "-", month.file, ".csv", sep = "")
+        write.csv(one_plot.month, file.path(path.out ,PLOT ,  file = filename), row.names = FALSE)
+        rows = rows + nrow(one_plot.month)
+        if(month.check == 12) month.check = 1 else(month.check = (month.check + 1))
+      }
     }
-  }
-} 
+  } 
+}
 # Seperating by chosen year
 year.check = 2016
 rows=1
-for (i in rows:nrow(one_plot.loop)){
-  Date.year <- one_plot.loop[i, "Date_Time"]
-  year.extract <- year(Date.year)
-  if (!is.na(year.extract)){
-    if (year.extract != year.check){
-      Date.min <- paste(year.extract, "-01-01 00:00:00", sep="")
-      Date.max <- paste(year.extract, "-12-31", " 23:59:59", sep="")
-      one_plot.year <- subset(one_plot.loop, Date_Time >= as.POSIXlt(Date.min) & Date_Time <= as.POSIXlt(Date.max))
-      filename <- paste(Plot.title,"-", year.extract, ".csv", sep = "")
-      write.csv(one_plot.year, file.path(path.out,  file = filename), row.names = FALSE)
-      rows = rows + nrow(one_plot.year)
-      year.check = (year.check + 1)
+for(PLOT in unique(comb$Plot_Name)){
+  month.check = 0
+  rows=1
+  #Making sure to only update the latest year
+  #This will get tricky around year change in winter
+  one_plot.loop <- comb[comb$Plot_Name == PLOT,]
+  
+  year <- year(Sys.Date())
+  
+  one_plot.loop <- one_plot.loop[one_plot.loop$Date_Time >= year, ]
+  
+  for (i in rows:nrow(one_plot.loop)){
+    Date.year <- one_plot.loop[i, "Date_Time"]
+    year.extract <- year(Date.year)
+    if (!is.na(year.extract)){
+      if (year.extract != year.check){
+        Date.min <- paste(year.extract, "-01-01 00:00:00", sep="")
+        Date.max <- paste(year.extract, "-12-31", " 23:59:59", sep="")
+        one_plot.year <- subset(one_plot.loop, Date_Time >= as.POSIXlt(Date.min) & Date_Time <= as.POSIXlt(Date.max))
+        filename <- paste(PLOT,"_", year.extract, ".csv", sep = "")
+        write.csv(one_plot.year, file.path(path.out, PLOT,  file = filename), row.names = FALSE)
+        rows = rows + nrow(one_plot.year)
+        year.check = (year.check + 1)
+      }
     }
   }
 }
-
 
