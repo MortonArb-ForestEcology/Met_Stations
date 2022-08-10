0f26c4aa4179f7460fb0fa72e578738dd72257f8
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-08-10 13:20:14 -0500

    Fixed yearly file seperation
    Yearly file seperation part of the script wasn't working. Provided a simpler working version.

--- Scripts/3_Met_File_Seperation.R
+++ Scripts/3_Met_File_Seperation.R
@@ -16,23 +16,15 @@ library(lubridate)
 library(tidyr)
 
 #Setting file paths
-
-path.personal <- "C:/Users/lfitzpatrick"
-path.data <- "/GitHub/Met_Stations/Data_raw_inputs/Single_Plots"
 path.met <- "G:/My Drive/East Woods/Rollinson_Monitoring/Data/Met Stations/Single_Plots/"
-path.out <- paste(path.met, "Data_clean/Clean_data", sep="")
+path.out <- paste(path.met, "Data_processed/Clean_data", sep="")
 setwd(path.out)
 
-
-#Currently using the Month_Separation script because combining the seperate files results in 147 columns
-#Will probably seperate the current month script into a QAQC script and the seperation script
-
 # Seperating by chosen year and month values
-
-plot.B127 <- read.csv("B127/B127.csv")
-plot.N115 <- read.csv("N115/N115.csv")
-plot.HH115 <- read.csv("HH115/HH115.csv")
-plot.U134 <- read.csv("U134/U134.csv")
+plot.B127 <- read.csv(file.path(path.out,"B127/B127.csv"))
+plot.N115 <- read.csv(file.path(path.out,"N115/N115.csv"))
+plot.HH115 <- read.csv(file.path(path.out,"HH115/HH115.csv"))
+plot.U134 <- read.csv(file.path(path.out,"U134/U134.csv"))
 
 
 comb <- rbind(plot.B127, plot.N115, plot.HH115, plot.U134)
@@ -44,7 +36,7 @@ for(PLOT in unique(comb$Plot_Name)){
   one_plot.loop <- comb[comb$Plot_Name == PLOT,]
   
   #Making sure we only update and add the neccessary files
-  old.files <- dir(file.path(path.met,"Data_clean/Clean_data" ,PLOT), ".csv")
+  old.files <- dir(file.path(path.met,"Data_processed/Clean_data" ,PLOT), ".csv")
   
   split <- strsplit(old.files, "_")
   
@@ -91,36 +83,24 @@ for(PLOT in unique(comb$Plot_Name)){
     }
   } 
 }
+
+
 # Seperating by chosen year
-year.check = 2016
-rows=1
 for(PLOT in unique(comb$Plot_Name)){
-  month.check = 0
-  rows=1
-  #Making sure to only update the latest year
-  #This will get tricky around year change in winter
   one_plot.loop <- comb[comb$Plot_Name == PLOT,]
   
   year <- year(Sys.Date())
   
-  one_plot.loop$Date_Time <- as.Date(one_plot.loop$Date_Time)
+  one_plot.loop$Date_Time <- as.POSIXct(one_plot.loop$Date_Time)
   
   one_plot.loop <- one_plot.loop[one_plot.loop$Date_Time >= year, ]
-  
-  for (i in rows:nrow(one_plot.loop)){
-    Date.year <- one_plot.loop[i, "Date_Time"]
-    year.extract <- year(Date.year)
-    if (!is.na(year.extract)){
-      if (year.extract != year.check){
-        Date.min <- paste(year.extract, "-01-01 00:00:00", sep="")
-        Date.max <- paste(year.extract, "-12-31", " 23:59:59", sep="")
-        one_plot.year <- subset(one_plot.loop, Date_Time >= as.POSIXlt(Date.min) & Date_Time <= as.POSIXlt(Date.max))
-        filename <- paste(PLOT,"_", year.extract, ".csv", sep = "")
-        write.csv(one_plot.year, file.path(path.out, PLOT,  file = filename), row.names = FALSE)
-        rows = rows + nrow(one_plot.year)
-        year.check = (year.check + 1)
-      }
-    }
+  for(YR in 2017:year){
+    Date.min <- paste(YR, "-01-01 00:00:00", sep="")
+    Date.max <- paste(YR, "-12-31", " 23:59:59", sep="")
+    one_plot.year <- subset(one_plot.loop, Date_Time >= as.POSIXlt(Date.min) & Date_Time <= as.POSIXlt(Date.max))
+    filename <- paste(PLOT,"_", YR, ".csv", sep = "")
+    write.csv(one_plot.year, file.path(path.out, PLOT,  file = filename), row.names = FALSE)
   }
 }
+
 
