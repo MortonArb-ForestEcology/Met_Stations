168b2ebe2278a843d034f650c330f881d1f39427
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-08-18 13:27:40 -0500

    Now I also make sure to create the previous year's full file

--- Scripts/3_Met_File_Seperation.R
+++ Scripts/3_Met_File_Seperation.R
@@ -83,8 +83,6 @@ for(PLOT in unique(comb$Plot_Name)){
   } 
 }
 
-#Need to also add a check for the year rolls over to make a final version for that year.
-
 
 # Seperating by chosen year
 for(PLOT in unique(comb$Plot_Name)){
@@ -94,6 +92,7 @@ for(PLOT in unique(comb$Plot_Name)){
   #Way of finding the oldest and most recent file to be deleted.
   old.plot <- dir.plot[match(1, stringr::str_detect(dir.plot, "up_to_"))]
   
+  #Deleting the old version
   if (file.exists(file.path(path.out, PLOT, old.plot))) {
     unlink(file.path(path.out, PLOT, old.plot))
     cat("The file is deleted")
@@ -106,9 +105,15 @@ for(PLOT in unique(comb$Plot_Name)){
   
   one_plot.loop$Date_Time <- as.POSIXct(one_plot.loop$Date_Time)
   
-  one_plot.year <- one_plot.loop[one_plot.loop$year <= year, ]
+  #Creating the previous year's complete file
+  one_plot.old <- one_plot.loop[one_plot.loop$year <= year-1 & one_plot.loop$year > year-2,]
+  write.csv(one_plot.old, file.path(path.out, PLOT,  file = paste(PLOT,"_" ,year-1, ".csv", sep="")), row.names = FALSE)
+  
+  one_plot.year <- one_plot.loop[one_plot.loop$year <= year & one_plot.loop$year > year-1, ]
   Date.min <- min(one_plot.year$Date_Time)
   Date.max <- max(one_plot.year$Date_Time)
+  
+  #Creating a file name that includes the ending of current measurements
   filename <- paste(PLOT,"_2022_up_to_" , as.Date(Date.max), ".csv", sep = "")
   write.csv(one_plot.year, file.path(path.out, PLOT,  file = filename), row.names = FALSE)
   
