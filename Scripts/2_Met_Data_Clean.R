e81dd1f209f11943b52f455dd6c1767e67092224
LucienFitzpatrick <lfitzpatrick@mortonarb.org>
2022-09-01 16:04:48 -0500

    Updated data cleaning
    Now we flag our problem values then clean them down the line, so our raw data is flagged but untouched

--- Scripts/2_Met_Data_Clean.R
+++ Scripts/2_Met_Data_Clean.R
@@ -2,12 +2,10 @@
 # Script by : Lucien Fitzpatrick
 # Project: Met_stations
 # Purpose: This is the primary script for the cleaning of Met station data previously harmonized
-#         To convert the raw data we harmonized into clean data
+#         To convert the raw data we harmonized into clean data. We save the flagged data back as raw flagged data
 # Inputs: Raw data from the data loggers stored in "G:/My Drive/East Woods/Rollinson_Monitoring/Data/Met Stations/Single_Plots/Harmonized_data"
-# Outputs: Clean data B127.csv
-#          U134.csv
-#          N115.csv
-#          HH115.csv
+# Outputs: Clean data B127.csv, U134.csv, N115.csv, HH115.csv
+#          Flagged harmonized data B127.csv, U134.csv, N115.csv, HH115.csv 
 # Notes: Cleaning in this script is defined as removing impossible values (negative soil moisture) and 
 #        removing measurements more than 4 standard deviations from the 2 week mean surrounding the date of measurement
 #         
@@ -18,12 +16,11 @@ library(ggplot2)
 library(lubridate)
 
 #Setting file paths
-path.met <- "G:/My Drive/East Woods/Rollinson_Monitoring/Data/Met Stations/Single_Plots/"
+path.met <- "G:/.shortcut-targets-by-id/0B_Fbr697pd36TkVHdDNJQ1dJU1E/East Woods/Rollinson_Monitoring/Data/Met Stations/Single_Plots/"
 path.out <- paste(path.met, "Data_processed/Harmonized_data", sep="")
 setwd(path.out)
 
 # Seperating by chosen year and month values
-
 plot.B127 <- read.csv("B127.csv")
 plot.N115 <- read.csv("N115.csv")
 plot.HH115 <- read.csv("HH115.csv")
@@ -34,6 +31,9 @@ comb$Date_Time <- as.POSIXct(comb$Date_Time)
 
 #Removing impossible soil_moissture readings
 comb$Soil_Moisture <- ifelse(comb$Soil_Moisture <= 0, NA, comb$Soil_Moisture)
+
+#Removing impossible air_temp readings
+comb$Air_Temp <- ifelse(comb$Air_Temp >= 150, NA, comb$Air_Temp)
 
 #Removing NA rows 
 comb <- comb[!is.na(comb$Plot_Name),]
@@ -51,17 +51,27 @@ for(PLOT in unique(comb$Plot_Name)){
     for(VAL in metrics){
       SM.mean <- mean(temp[temp$Date %in% DATES, VAL], na.rm = T)
       SM.sd <- sd(temp[temp$Date %in% DATES, VAL], na.rm = T)
-      temp[temp$Date == DOY, "SIGFLAG"] <- ifelse(temp[temp$Date == DOY, VAL]<SM.mean-4*SM.sd | temp[temp$Date == DOY, VAL]>SM.mean+4*SM.sd, T, F)
+      temp[temp$Date == DOY, paste0("SIGFLAG_", VAL)] <- ifelse(temp[temp$Date == DOY, VAL]<SM.mean-4*SM.sd | temp[temp$Date == DOY, VAL]>SM.mean+4*SM.sd, T, F)
     }
   }
   df.clean <- rbind(df.clean, temp)
 }
 
-#Removing observations that are flagged as illogical
-df.clean$SIGFLAG <- ifelse(is.na(df.clean$SIGFLAG), F, (df.clean$SIGFLAG) ) 
+#Saving the flagged data into individual plot files
+for(PLOT in unique(df.clean$Plot_Name)){
+  temp <- df.clean[df.clean$Plot_Name == PLOT,]
+  path.fin <- paste(path.met, "Data_processed/Harmonized_data/", sep="")
+  filename <- paste(PLOT, ".csv", sep = "")
+  write.csv(temp, file.path(path.fin,  file = filename), row.names = FALSE)
+}
 
-df.clean <- df.clean[which(df.clean$SIGFLAG == F),]
-df.clean$Date_Time <- as.POSIXct(df.clean$Date_Time)
+#Removing our flagged values to create a clean dataframe.
+df.clean$Soil_Moisture <- ifelse(df.clean$SIGFLAG_Soil_Moisture == T, NA, df.clean$Soil_Moisture )
+df.clean$Soil_Temp <- ifelse(df.clean$SIGFLAG_Soil_Temp== T, NA, df.clean$Soil_Temp )
+df.clean$Air_Temp <- ifelse(df.clean$SIGFLAG_Air_Temp== T, NA, df.clean$Air_Temp )
+df.clean$Relative_Humidity <- ifelse(df.clean$SIGFLAG_Relative_Humidity== T, NA, df.clean$Relative_Humidity )
+df.clean$PAR <- ifelse(df.clean$SIGFLAG_PAR== T, NA, df.clean$PAR)
+
 
 #Saving the clean data into individual plot files
 for(PLOT in unique(df.clean$Plot_Name)){
